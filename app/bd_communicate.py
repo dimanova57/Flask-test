525b1e84a51514c5fc588248e54727a34a990f98
dimanova57 <dimanova140810@gmail.com>
2023-02-19 11:04:59 +0200

    message

--- app/bd_communicate.py
+++ app/bd_communicate.py
@@ -1,14 +1,16 @@
 import sqlite3
+from random import shuffle
+
 from app import app
 from flask import g
 
 
-def add_question(question, ans1, ans2, correct):
-    msg = ''
-    connection = sqlite3.connect("app/app.db")
+def add_question(test, question, ans1, ans2, correct):
+    connection = sqlite3.connect('app/app.db', check_same_thread=False)
     cursor = connection.cursor()
+    msg = ''
     try:
-        cursor.execute(f"""INSERT INTO chess_test
+        cursor.execute(f"""INSERT INTO {test}
                         (question, ans1, ans2, correct) VALUES
                         {(question, ans1, ans2, correct)}""")
         connection.commit()
@@ -21,11 +23,43 @@ def add_question(question, ans1, ans2, correct):
         return msg
 
 
-def get_all_tests():
-    connection = sqlite3.connect("app/app.db")
+def get_all_tests(table_name):
+    connection = sqlite3.connect('app/app.db', check_same_thread=False)
     cursor = connection.cursor()
-    cursor.execute("SELECT * from chess_test")
-    return cursor.fetchall()
+    cursor.execute(f'SELECT * from {table_name}')
+    all_tests = cursor.fetchall()
+    mixed_tests = {}
+    for test in all_tests:
+        l = list({test[1]: False, test[2]: False, test[3]: True}.items())
+        shuffle(l)
+        d_shuffled = dict(l)
+        mixed_tests[test[0]] = d_shuffled
+
+        print(d_shuffled)
+    print(mixed_tests)
+    return mixed_tests
+
+
+def create_new_table(name):
+    connection = sqlite3.connect('app/app.db', check_same_thread=False)
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f'CREATE TABLE {name} (question TEXT, ans1 TEXT, ans2 TEXT, correct TEXT)')
+        msg = 'SUCCESS'
+    except Exception as er:
+        msg = str(er)
+    return msg
+
+
+def delete_new_table(name):
+    connection = sqlite3.connect('app/app.db', check_same_thread=False)
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f'DROP TABLE {name}')
+        msg = 'SUCCESS'
+    except:
+        msg = 'ERROR'
+    return msg
 
 
 @app.teardown_appcontext
@@ -33,3 +67,10 @@ def close_connection(ex):
     db = getattr(g, "_database", None)
     if db:
         db.close()
+
+
+def get_all_table_names():
+    connection = sqlite3.connect('app/app.db', check_same_thread=False)
+    cursor = connection.cursor()
+    tables = cursor.execute("SELECT name FROM sqlite_schema WHERE type ='table'")
+    return tables.fetchall()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, flash, redirect
 from app import app
-from app.bd_communicate import add_question, get_all_tests
+from app.bd_communicate import *
+
+last_test = ''
 
 
 @app.route('/')
@@ -11,18 +13,49 @@ def index():
 @app.route('/add_test', methods=['POST', 'GET'])
 def add_test():
     if request.method == 'POST':
+        selected_test = request.form['selected']
         question = request.form['question']
         ans1 = request.form['ans1']
         ans2 = request.form['ans2']
         correct = request.form['correct']
-        msg = add_question(question, ans1, ans2, correct)
+        msg = add_question(selected_test, question, ans1, ans2, correct)
 
         flash(msg)
         redirect('/add_test')
-    return render_template("add_test.html")
+    return render_template("add_test.html", tablenames=get_all_table_names())
 
 
-@app.route('/pass_test', methods=["POST", "GET"])
-def pass_test():
-    all_tests = get_all_tests()
+@app.route('/delete_table', methods=["POST"])
+def delete_table():
+    selected_table = request.form['delete_selected']
+    delete_new_table(selected_table)
+
+
+@app.route('/add_table', methods=["POST"])
+def new_table():
+    name = request.form['tablename']
+    res = create_new_table(name)
+    print(res)
+    return redirect('/add_test')
+
+
+@app.route('/pass_test_result', methods=['POST'])
+def pass_test_result():
+    all_tests = get_all_tests(last_test)
+    user_answers = request.form
+    correct = 0
+    for question in user_answers:
+        user_answer = user_answers[question]
+        print(user_answer)
+        ans = all_tests[question][user_answer]
+        if ans:
+            correct += 1
+    return f"correct -> {correct}"
+
+
+@app.route('/pass_test/<test_name>')
+def pass_test(test_name):
+    global last_test
+    last_test = test_name
+    all_tests = get_all_tests(test_name)
     return render_template('pass_test.html', all_tests=all_tests)
